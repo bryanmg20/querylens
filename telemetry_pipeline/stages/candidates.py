6379e446c08d7dcfe69170efe75cfe647fa9e2e5
bryan <bemontoya@uninorte.edu.co>
2026-09-14 16:47:08 -0500

    refactor: extract selector functions into stages/selectors

---
+++ telemetry_pipeline/stages/candidates.py
@@ -0,0 +1,13 @@
+import selectors
+class CandidatesStage:
+    def __init__(self, collector):
+        self.db_connection = collector
+
+    def execute(self, stats):
+        stats = self.collector.preprocess_statements(stats)
+        selectors.select_high_impact_time_statements(stats)
+        selectors.select_unstable_statements(stats)
+        selectors.select_disk_spill_indicator(stats)
+        selectors.select_candidates_to_explain(stats)
+        selectors.select_explain_ready(stats)
+        return stats
